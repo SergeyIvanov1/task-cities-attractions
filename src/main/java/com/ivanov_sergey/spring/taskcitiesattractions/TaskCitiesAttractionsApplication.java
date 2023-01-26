24fced731cfb3a84d63c19186be5ab1f5a419fc7
Sergey Ivanov <ivanovsergey.samara@gmail.com>
2023-01-26 17:17:39 +0400

    init commit

---
+++ src/main/java/com/ivanov_sergey/spring/taskcitiesattractions/TaskCitiesAttractionsApplication.java
@@ -0,0 +1,13 @@
+package com.ivanov_sergey.spring.taskcitiesattractions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskCitiesAttractionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskCitiesAttractionsApplication.class, args);
+    }
+
+}
